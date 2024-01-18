2b951b7c31e2a812cba742edc8ebacfd9a86c399
BhargavChavda <bhargavchavda005@gmail.com>
2024-01-18 14:41:43 +0530

    Gets the current weather for a city input and stores the output in weather.log

--- weather.sh
+++ weather.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
-echo 'starting'
-city=Indore
+echo 'Enter City Name:'
+read city
 
-curl -s wttr.in/$city?
+curl -s wttr.in/$city?T --output weather.log
+
+obs_temp=$(curl -s wttr.in/$city?T | grep -m 1 '°.' | grep -Eo -e '-?[[:digit:]].*')
+echo "Current temperature of $city is $obs_temp"
